bf2f1a2d477a47f6fc9d3d49d71d362aea32f4b3
Matt Banack <matt@banack.net>
2013-01-18 23:38:44 -0700

    Didn't like the symlink

--- pickrandomline.py
+++ pickrandomline.py
@@ -1,1 +1,24 @@
-/home/vorok/bin/pickrandomline.py
+#!/usr/bin/python
+import sys
+import random
+
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-q", "--quote", action="store_true",
+              help="wrap output in single quotes to guard against spaces")
+(options, args) = parser.parse_args()
+
+lines = []
+l = sys.stdin.readline()
+while l:
+    l = l.strip()
+    if l != ".":
+        lines.append(l)
+    l = sys.stdin.readline()
+
+if options.quote:
+    sys.stdout.write("'" + random.choice(lines) + "'")
+else:
+    sys.stdout.write(random.choice(lines))
+
+
